d715707e88408aca78861a8961786d855292d184
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2021-10-31 02:26:37 +0200

    movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,18 @@ public class Player : MonoBehaviour
 {
 
     Rigidbody2D rbgd;
-    float speed = 200;
+    float speed = 800;
+    float posY, gravity;
+    bool groundDetect = true;
 
     void Start() {
         rbgd = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-        //Vector3 point = new Vector3(Input.mousePosition.x, Input.mousePosition.y, +10);
-        float posX = Input.GetAxis("Horizontal");
-        float posY = Input.GetAxis("Vertical");
+        float posX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 
-        Vector2 pos = new Vector2(posX, posY) * Time.deltaTime * speed;
 
-        Vector2 point = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-        float rotationZ = Mathf.Atan2(Input.mousePosition.x, Input.mousePosition.y);
-
-        rbgd.transform.rotation = Quaternion.Euler(0, 0, rotationZ);
-        rbgd.velocity = pos;
-
+        rbgd.velocity = new Vector2(posX, posY);
     }
 }
