122ab1aadf0dd26e23971789813240b8d5e33d5d
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2021-10-31 02:38:18 +0200

    shit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,35 @@ public class Player : MonoBehaviour
 {
 
     Rigidbody2D rbgd;
+    SpriteRenderer sprite;
     float speed = 800;
     float posY, gravity;
-    bool groundDetect = true;
+    bool death;
 
     void Start() {
         rbgd = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update() {
         float posX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 
+        if(Input.GetKey(KeyCode.A)) {
+            sprite.flipX = true;
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            sprite.flipX = false;
+        }
 
+
+
+        posY = rbgd.velocity.y;
         rbgd.velocity = new Vector2(posX, posY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag == "death") {
+            death = true;
+        }
     }
 }
---
+++ Assets/Scripts/platforms.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platforms : MonoBehaviour
+{
+    public GameObject platform;
+    float targetTimer = 20;
+
+    void Start()
+    {
+            
+    }
+
+    void Update() {
+        if(!targetTimer < 0) {
+               
+        }
+    }
+}
